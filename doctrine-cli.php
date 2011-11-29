953d47dee6a8f5466d3b8e9d0d94fc1a620d4407
Marco Pivetta <ocramius@gmail.com>
2011-11-29 17:24:06 +0100

    Fixing wrong import

--- doctrine-cli.php
+++ doctrine-cli.php
@@ -2,7 +2,7 @@
 use Symfony\Component\Console\Helper\HelperSet,
     Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper,
     Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper,
-    \Doctrine\ORM\Tools\Console\ConsoleRunner;
+    Doctrine\ORM\Tools\Console\ConsoleRunner;
 require_once __DIR__ . '/bootstrap.php';
 
 $helperSet = new HelperSet(array(
