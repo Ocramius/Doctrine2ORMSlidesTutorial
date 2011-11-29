39e9d9a6db264578112ac6f084c125aa82a9c7f4
Marco Pivetta <ocramius@gmail.com>
2011-11-29 17:12:22 +0100

    Changing bootstrap process (standard annotation loader)

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,11 @@
 use Doctrine\ORM\Tools\Setup,
     Doctrine\ORM\EntityManager,
     Doctrine\ORM\Configuration,
-    Doctrine\Common\Cache\ArrayCache as Cache;
+    Doctrine\Common\Cache\ArrayCache as Cache,
+    Doctrine\Common\Annotations\AnnotationRegistry;
+
+ini_set('display_errors', 'On');
+ini_set('error_reporting', E_ERROR | E_RECOVERABLE_ERROR);
 
 //autoloading
 require_once 'library/doctrine-orm/lib/Doctrine/ORM/Tools/Setup.php';
@@ -18,8 +22,12 @@ $config->setProxyNamespace('EntityProxy');
 $config->setAutoGenerateProxyClasses(true);
 
 //mapping (example uses annotations, could be any of XML/YAML or plain PHP)
-$driverImpl = $config->newDefaultAnnotationDriver(__DIR__ . '/library/Entity');
-$config->setMetadataDriverImpl($driverImpl);
+AnnotationRegistry::registerFile(__DIR__ . '/library/doctrine-orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+$driver = new Doctrine\ORM\Mapping\Driver\AnnotationDriver(
+    new Doctrine\Common\Annotations\AnnotationReader(),
+    array(__DIR__ . '/library/Entity')
+);
+$config->setMetadataDriverImpl($driver);
 $config->setMetadataCacheImpl($cache);
 
 //getting the EntityManager
