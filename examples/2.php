e7437c08786a987b0acf48d1952ba65c779d83b1
Marco Pivetta <ocramius@gmail.com>
2011-11-29 18:16:21 +0100

    Better output

--- examples/2.php
+++ examples/2.php
@@ -7,7 +7,8 @@ $greeting = $em->find('Entity\Greeting', 1);
 
 if($greeting) {
     //The EntityManager has already provided us an object of type Entity\Greeting!
-    echo $greeting->getContent();
+    echo 'Found a greeting (instance of ' . get_class($greeting)
+        . ') with content ' . $greeting->getContent();
 }else{
     echo 'Couldn\'t find Greeting with id=1';
 }
